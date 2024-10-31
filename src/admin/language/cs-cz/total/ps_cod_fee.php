2c92d041ce4b49e379af68098384b46fdd8d8a09
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-10-31 11:07:16 +0100

    added tabs

--- src/admin/language/cs-cz/total/ps_cod_fee.php
+++ src/admin/language/cs-cz/total/ps_cod_fee.php
@@ -1,11 +1,23 @@
 <?php
 // Heading
 $_['heading_title']              = 'Playful Sparkle - Poplatek za platbu na dobírku';
+$_['heading_getting_started']    = 'Začínáme';
+$_['heading_setup']              = 'Nastavení poplatek za platbu na dobírku';
+$_['heading_faq']                = 'Často kladené otázky';
+$_['heading_contact']            = 'Kontaktujte podporu';
 
 // Text
 $_['text_extension']             = 'Rozšíření';
 $_['text_success']               = 'Úspěch: Upravil(a) jste celkový poplatek za platbu na dobírku!';
 $_['text_edit']                  = 'Upravit celkový poplatek za platbu na dobírku';
+$_['text_getting_started']       = '<p><strong>Přehled:</strong> Rozšíření Playful Sparkle - Poplatek za dobírku pro OpenCart 4 umožňuje majitelům obchodů automaticky přidávat konfigurovatelný poplatek k objednávkám dokončeným pomocí platební metody Dobírka (COD). To pomáhá kompenzovat transakční náklady nebo další poplatky spojené s dobírkou, čímž se zvyšuje flexibilita a kontrola nad konečnými celkovými částkami objednávek.</p><p><strong>Požadavky:</strong> OpenCart 4.0+, PHP 7.4 nebo vyšší.</p>';
+$_['text_setup']                 = '<p>Pro konfiguraci zadejte požadovanou částku poplatku, která se má použít při pokladně, vyberte příslušnou daňovou třídu a aktivujte rozšíření. Jakmile bude nastavena, poplatek bude automaticky zahrnut, když si zákazník zvolí Dobírku (COD) během pokladny.</p>';
+$_['text_faq']                   = '<details><summary>Je částka poplatku uvedena s daní nebo bez daně?</summary>Poplatek se uplatňuje včetně daně. Například pokud nastavíte poplatek na 5 EUR, 5 EUR bude přidáno k celkové částce objednávky včetně daně.</details><details><summary>Mohu použít různé poplatky pro jiné platební metody?</summary>Ne, toto rozšíření uplatňuje poplatek pouze při výběru platební metody Dobírka (COD).</details><details><summary>Objeví se poplatek odděleně v souhrnu objednávky?</summary>Ano, poplatek se zobrazuje jako samostatná položka v rozpisu celkové částky objednávky během pokladny.</details>';
+$_['text_contact']               = '<p>Pro další pomoc se prosím obraťte na náš tým podpory:</p><ul><li><strong>Kontakt:</strong> <a href="mailto:%s">%s</a></li><li><strong>Dokumentace:</strong> <a href="https://github.com/playfulsparkle/oc4_google_tag_manager" target="_blank" rel="noopener noreferrer">Dokumentace pro uživatele</a></li></ul>';
+
+// Tab
+$_['tab_general']                = 'Obecné';
+$_['tab_help_and_support']       = 'Nápověda a podpora';
 
 // Entry
 $_['entry_fee']                  = 'Poplatek';
--- src/admin/language/sk-sk/total/ps_cod_fee.php
+++ src/admin/language/sk-sk/total/ps_cod_fee.php
@@ -1,11 +1,23 @@
 <?php
 // Heading
 $_['heading_title']              = 'Playful Sparkle - Poplatok za dobierku';
+$_['heading_getting_started']    = 'Začíname';
+$_['heading_setup']              = 'Nastavenie poplatok za dobierku';
+$_['heading_faq']                = 'Často kladené otázky';
+$_['heading_contact']            = 'Kontaktujte podporu';
 
 // Text
 $_['text_extension']             = 'Rozšírenia';
 $_['text_success']               = 'Úspech: Upravil(a) ste celkový poplatok za dobierku!';
 $_['text_edit']                  = 'Upraviť celkový poplatok za dobierku';
+$_['text_getting_started']       = '<p><strong>Prehľad:</strong> Rozšírenie Playful Sparkle - COD Fee pre OpenCart 4 umožňuje majiteľom obchodov automaticky pridať konfigurovateľný poplatok k objednávkam dokončeným pomocou platobnej metódy Dobierka (COD). To pomáha kompenzovať transakčné náklady alebo dodatočné poplatky spojené s COD, čím sa zvyšuje flexibilita a kontrola nad konečnými súčtami objednávok.</p><p><strong>Požiadavky:</strong> OpenCart 4.0+, PHP 7.4 alebo novší.</p>';
+$_['text_setup']                 = '<p>Aby ste rozšírenie nastavili, zadajte požadovanú výšku poplatku, ktorý sa má uplatniť pri pokladni, vyberte príslušnú daňovú triedu a aktivujte rozšírenie. Po nastavení bude poplatok automaticky zahrnutý, keď si zákazník zvolí Dobierku (COD) počas pokladne.</p>';
+$_['text_faq']                   = '<details><summary>Je poplatok zadaný s daňou alebo bez dane?</summary>Poplatok sa uplatňuje vrátane dane. Napríklad, ak nastavíte poplatok na 5 EUR, 5 EUR bude pridané k celkovému súčtu objednávky vrátane dane.</details><details><summary>Môžem uplatniť rôzne poplatky pre iné platobné metódy?</summary>Nie, toto rozšírenie uplatňuje poplatok iba v prípade, že je zvolená platobná metóda Dobierka (COD).</details><details><summary>Bude poplatok zobrazený samostatne v súhrne objednávky?</summary>Áno, poplatok je zobrazený ako samostatná položka v rozdelení celkového súčtu objednávky počas pokladne.</details>';
+$_['text_contact']               = '<p>További segítségért, kérjük, lépjen kapcsolatba támogatói csapatunkkal:</p><ul><li><strong>Kapcsolat:</strong> <a href="mailto:%s">%s</a></li><li><strong>Dokumentáció:</strong> <a href="https://github.com/playfulsparkle/oc4_google_tag_manager" target="_blank" rel="noopener noreferrer">Felhasználói dokumentáció</a></li></ul>';
+
+// Tab
+$_['tab_general']                = 'Všeobecné';
+$_['tab_help_and_support']       = 'Pomoc &amp; Podpora';
 
 // Entry
 $_['entry_fee']                  = 'Poplatok';
